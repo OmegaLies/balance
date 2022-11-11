747d0b14b9ea9da956d7841903e4cf1a3048035a
omegalies <omegaunderwall@gmail.com>
2022-11-11 16:58:42 +0300

    Added site availability check

--- main.rb
+++ main.rb
@@ -4,11 +4,17 @@ require_relative "lib/balance"
 
 URL = "http://www.cbr.ru/scripts/XML_daily.asp".freeze
 
-response = Net::HTTP.get_response(URI.parse(URL))
-doc = REXML::Document.new(response.body)
+begin
+  response = Net::HTTP.get_response(URI.parse(URL))
+  doc = REXML::Document.new(response.body)
+  dollar_rate = REXML::XPath.first(doc, '//Valute[@ID="R01235"]').get_text('Value').to_s.gsub(',', '.').to_f
+  puts "Курс доллара: #{dollar_rate}"
+rescue SocketError
+  puts "Сайт Центробанка недоступен. Проверьте интернет соединение"
+  puts "Введите курс доллара"
+  dollar_rate = STDIN.gets.to_f
+end
 
-dollar_rate = REXML::XPath.first(doc, '//Valute[@ID="R01235"]').get_text('Value').to_s.gsub(',', '.').to_f
-puts "Курс доллара: #{dollar_rate}"
 puts "Сколько у вас рублей?"
 input_rubles = gets.to_f.round(2)
 puts "Сколько у вас долларов?"
